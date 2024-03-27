4dc11b0b8ae70caf03f33f09ef22a2f32bef5b8e
Ashish Chauhan <119170180+Ash914027@users.noreply.github.com>
2024-03-27 23:50:45 +0530

    second commit

---
+++ JAVAScript/INT222.js
@@ -0,0 +1,7 @@
+
+// Arrays
+const fruits = ["apple", "banana", "orange"];
+
+console.log(fruits[0]); // Output: apple
+console.log(fruits[1]); // Output: banana
+console.log(fruits[2]); // Output: orange
